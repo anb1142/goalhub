68de95c7d615d950253d2d2aab95ca57e3c9b857
Anuja Nimesh <anujanimesh@gmail.com>
2023-06-26 20:31:02 +0530

    fix: frontend .ts init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<>
+			<h1>Goalsapp</h1>
+		</>
+	);
+}
+
+export default App;
--- frontend/src/components/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-function Spinner() {
-	return (
-		<div className="spinnerContainer">
-			<div className="spinner"></div>
-		</div>
-	);
-}
-
-export default Spinner;
