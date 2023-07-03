90eeeb6f8767c65ed61fb77cbdf76ba73d1bbb18
Anuja Nimesh <anujanimesh@gmail.com>
2023-07-03 16:10:01 +0530

    feat: switched to axios interceptors from

--- frontend/src/utils/http.ts
+++ frontend/src/utils/http.ts
@@ -1,18 +1,43 @@
-import axios from "axios";
-
+import axios, { AxiosError } from "axios";
 import Cookies from "js-cookie";
+// import { enqueueSnackbar } from "notistack";
+import { toast } from "react-toastify";
 import { ISignInResponseDto } from "../services/auth/auth.type";
 
-const cookiedata = Cookies.get("user");
-const user: ISignInResponseDto["data"] = cookiedata
-	? JSON.parse(cookiedata)
-	: null;
+const baseUrl = "http://localhost:5000/api";
 
 const http = axios.create({
-	baseURL: "http://localhost:5000/api",
-	headers: {
-		Authorization: `Bearer ${user ? user.token : ""}`,
-	},
+	baseURL: baseUrl,
 });
+
+http.interceptors.request.use(
+	async (config) => {
+		const cookiedata = Cookies.get("user");
+		const user: ISignInResponseDto["data"] = cookiedata
+			? JSON.parse(cookiedata)
+			: null;
+		try {
+			config.headers.Authorization = `Bearer ${user ? user.token : ""}`;
+			return config;
+		} catch (error) {
+			throw error;
+		}
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
+);
+
+http.interceptors.response.use(
+	(res) => res,
+	(error) => {
+		console.log(error);
+		if (error instanceof AxiosError) {
+			const msg = error.response?.data.message;
+			toast.error(msg ? msg : error.message);
+		}
+		return Promise.reject(error);
+	}
+);
 
 export default http;
