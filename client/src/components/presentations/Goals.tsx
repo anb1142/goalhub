9cad3e4a0aabe560aad14f29986510f8f3f88a43
Anuja Nimesh <anujanimesh@gmail.com>
2023-11-22 19:01:53 +0530

    seperated DoneList

--- client/src/components/presentations/Goals.tsx
+++ client/src/components/presentations/Goals.tsx
@@ -5,6 +5,12 @@ import { IGoals } from "../../services/goals/goal.type";
 import { GoalList } from "./GoalList";
 import GoalsSkeleton from "./GoalsSkeleton";
 
+type ICompletedListProps = {
+	done: IGoals;
+	open: boolean;
+	setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
 type IGoalsProps = {
 	goals: IGoals;
 	todo: IGoals;
@@ -14,44 +20,44 @@ type IGoalsProps = {
 	open: boolean;
 	setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
+const DoneList = (props: ICompletedListProps) => (
+	<>
+		<Box sx={{ my: 2 }} onClick={() => props.setOpen((prev) => !prev)}>
+			<Typography
+				sx={{
+					fontSize: 14,
+					color: "gray",
+					width: "100%",
+					cursor: "pointer",
+					display: "flex",
+					justifyContent: "space-between",
+				}}
+			>
+				Completed
+				<Box>{props.open ? <ExpandLess /> : <ExpandMore />}</Box>
+			</Typography>
+			<Divider />
+		</Box>
+		<Collapse in={props.open}>
+			<GoalList goals={props.done} />
+		</Collapse>
+	</>
+);
+
 function Goals(props: IGoalsProps) {
-	// TODO reduce nesting
-	return (
-		<>
-			{props.goals.length > 0 ? (
-				<>
-					<GoalList goals={props.todo} />
-					{props.done.length > 0 && (
-						<>
-							<Box sx={{ my: 2 }} onClick={() => props.setOpen((prev) => !prev)}>
-								<Typography
-									sx={{
-										fontSize: 14,
-										color: "gray",
-										width: "100%",
-										cursor: "pointer",
-										display: "flex",
-										justifyContent: "space-between",
-									}}
-								>
-									Completed
-									<Box>{props.open ? <ExpandLess /> : <ExpandMore />}</Box>
-								</Typography>
-								<Divider />
-							</Box>
-							<Collapse in={props.open}>
-								<GoalList goals={props.done} />
-							</Collapse>
-						</>
-					)}
-				</>
-			) : !props.fetched && props.isLoading ? (
-				<GoalsSkeleton />
-			) : (
-				<Typography textAlign="center">You got no goals</Typography>
-			)}
-		</>
-	);
+	if (props.goals.length > 0) {
+		return (
+			<>
+				<GoalList goals={props.todo} />
+				{props.done.length > 0 && (
+					<DoneList open={props.open} setOpen={props.setOpen} done={props.done} />
+				)}
+			</>
+		);
+	} else if (!props.fetched && props.isLoading) {
+		return <GoalsSkeleton />;
+	}
+	return <Typography textAlign="center">You got no goals</Typography>;
 }
 
 export default Goals;
